ef9e56f5d6e157ff2f1a04856d39b8d44656ba05
DiegoPerezPonce <diegoperezponce96@gmail.com>
2026-02-17 16:26:33 +0100

    Add alias field to actors table and seed data #2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,9 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
-            
+            ActorFakerSeeder::class,
+            FilmActorSeeder::class,
+            FilmFakerSeeder::class
         ]);
     }
 }
